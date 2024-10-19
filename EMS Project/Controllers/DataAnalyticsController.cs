312d4334957cab1164c99b3fd0892ba84795dd01
Deltas Lephalala <lephalad@legal.regn.net>
2024-10-19 22:04:58 +0200

    Changed the project name from Organisation_X to EMS
    - Refactored the project code, files and folders
    - Added necessary namespaces and tag helpers.

---
+++ EMS Project/Controllers/DataAnalyticsController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EMS.Controllers
+{
+    public class DataAnalyticsController : Controller
+    {
+        [Authorize(Policy = "readonlypolicy")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        /*public IActionResult sqlMethod()
+        {
+            Organisation_DB _context = new Organisation_DB();
+
+            var sqlQuery = _context.Employees.JobRole
+                .GroupBy(g => g.Employees.Gender)
+                .selext(g => new(nameof g.key, count = g.count()));
+            return View();
+        }*/
+    }
+}
